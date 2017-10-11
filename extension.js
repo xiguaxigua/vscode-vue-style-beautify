5a69fee11473f4129d9191c2acb57b16f7c83ba5
quietcoder <1642965215@qq.com>
2017-10-11 10:51:18 +0800

    add: output function

--- extension.js
+++ extension.js
@@ -1,14 +1,25 @@
 const vscode = require('vscode');
 
+let output = null;
+
+function showOutput (msg) {
+  if (!output) output = vscode.window.createOutputChannel('vueStyle');
+  output.clear();
+  output.appendLine('[vueStyle]:');
+  output.append(msg);
+  output.show();
+}
+
 function activate(context) {
   const onCommand = vscode.commands.registerTextEditorCommand('vueStyle.beautify', function (textEditor) {
     vscode.window.showInformationMessage('Hello World!');
-    console.log(textEditor)
+    console.log(textEditor);
+    console.log(showOutput);
   });
 
   const onSave = vscode.workspace.onWillSaveTextDocument((event) => {
     vscode.window.showInformationMessage('save Hello World!');
-    console.log(event)
+    console.log(event);
   });
 
   context.subscriptions.push(onCommand);
