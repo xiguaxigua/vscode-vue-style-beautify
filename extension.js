d0794b005923a82954a856da96433bf5c02ada71
quietcoder <1642965215@qq.com>
2017-10-16 17:34:08 +0800

    add: block generator

--- extension.js
+++ extension.js
@@ -1,7 +1,9 @@
 const vscode = require('vscode');
 
-let output = null;
+const EMBEDDED_FILE_SUPPORT = ['html', 'htm', 'vue', 'vue-html']
+const CSS_FILE_SUPPORT = ['css', 'less', 'scss', 'sass', 'sass-indented']
 
+let output = null;
 function showOutput (msg) {
   if (!output) output = vscode.window.createOutputChannel('vueStyle');
   output.clear();
@@ -10,9 +12,78 @@ function showOutput (msg) {
   output.show();
 }
 
+function getEmbeddedBlock (args) {
+  const { document } = args;
+  let content = null;
+  let pos = 0;
+  let char = '';
+  let startIndex = -1;
+  let syntax = 'css';
+  const text = document.getText();
+  const textLangth = text.length;
+  while (pos < textLangth) {
+    char = text.charAt(pos);
+    if (char === '<' && text.substr(pos, 6) === '<style') {
+      let tag = '';
+      do {
+        char = text.charAt(pos);
+        tag += char;
+        pos++
+      } while (char !== '>' && pos < text.length);
+
+      const matchedSyntax = tag.match(/lang=['"](.+)?['"]/);
+      syntax = matchedSyntax[1] || 'css'
+
+      startIndex = pos + 1;
+    }
+
+    if (char === '<' && text.substr(pos, 8) === '</style>') {
+      content = text.substring(startIndex, pos)
+      const start = document.positionAt(startIndex);
+      const end = document.positionAt(pos);
+      const range = new vscode.Range(start, end)
+
+      return { range, content, syntax }
+    }
+
+    pos++;
+  }
+}
+
+function getCssBlock (args) {
+  const { document, selection } = args;
+  let range = null;
+  let content = null;
+
+  if (!selection || (selection && selection.isEmpty)) {
+    const lastLine = this.document.lineAt(this.document.lineCount - 1);
+    const start = new vscode.Position(0, 0);
+    const end = new vscode.Position(document.lineCount - 1, lastLine.text.length);
+    range = new vscode.Range(start, end);
+    content = document.getText();
+  } else {
+    range = new vscode.Range(selection.start, selection.end);
+    content = document.getText(range);
+  }
+
+  return { range, content, syntax: 'css' };
+}
+
 function activate(context) {
   const onCommand = vscode.commands.registerTextEditorCommand('vueStyle.beautify', function (textEditor) {
     vscode.window.showInformationMessage('Hello World!');
+    const { document, selection } = textEditor;
+    const { languageId } = document;
+    let block = {};
+
+    if (~EMBEDDED_FILE_SUPPORT.indexOf(languageId)) {
+      block = getEmbeddedBlock({ document });
+    } else if (~CSS_FILE_SUPPORT.indexOf(languageId)) {
+      block = getCssBlock({ document, selection });
+    } else {
+      return showOutput(`${ languageId } syntac not support now.`);
+    }
+    console.log(block);
     console.log(textEditor);
     console.log(showOutput);
   });
