59639a52034123615c79ad1e469a1efa06231d27
quietcoder <1642965215@qq.com>
2018-01-02 11:19:39 +0800

    feat(csscomb-settings): update order

--- example/cssnext.vue
+++ example/cssnext.vue
@@ -1,9 +1,17 @@
 <template>
 </template>
 
-<style>
+<style lang="less">
 body {
   width: 10px;
   height: 10px;
+  cursor: pointer;
+
+  p {
+    font-size: 10px;
+    -webkit-transform: translate(1);
+    transform: translate(1);
+    display: inline;
+  }
 }
 </style>
